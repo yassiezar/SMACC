a1e5d051c6993ce24655a2ca9bc71c5d7a850d99
Pabo Iñigo Blasco <pblasco@plainconcepts.com>
2020-06-23 20:28:33 +0200

    adding more tests to calendar atomic and sm_dance_bot_strikes_back(disabled)

--- smacc_diagnostics/smacc_runtime_test/src/smacc_runtime_test_node.cpp
+++ smacc_diagnostics/smacc_runtime_test/src/smacc_runtime_test_node.cpp
@@ -31,8 +31,8 @@ public:
   ros::NodeHandle nh_;
   ros::Subscriber rosoutSub_;
   ros::Subscriber smaccTransitionSub_;
+  bool cleanSuccessExit_ = false;
 
-  
   std::vector<std::shared_ptr<TestPolicy>> testPolicies;
   boost::signals2::signal<void(const rosgraph_msgs::Log&)> onRosOutMsg;
   boost::signals2::signal<void(const smacc_msgs::SmaccTransitionLogEntry&)> onSmaccTransition;
@@ -70,7 +70,8 @@ public:
     }
 
     rosoutSub_ = nh_.subscribe("/rosout", 1, &SmaccTestRuntimeNode::onRosOutCallback, this);
-    smaccTransitionSub_ = nh_.subscribe(stateMachineROSParamWs + "/smacc/transition_log", 1, &SmaccTestRuntimeNode::onSmaccTransitionCallback, this);
+    smaccTransitionSub_ = nh_.subscribe(stateMachineROSParamWs + "/smacc/transition_log", 1,
+                                        &SmaccTestRuntimeNode::onSmaccTransitionCallback, this);
 
     XmlRpc::XmlRpcValue successSwitchParam;
     private_nh.getParam("success_switch", successSwitchParam);
@@ -99,7 +100,7 @@ public:
   void sucess(std::string msg)
   {
     ROS_INFO_STREAM("SmaccRuntimeTestNode policy thrown success exit: " << msg);
-    exit(-1);
+    cleanSuccessExit_ = true;
   }
 
   void failure(std::string msg)
@@ -118,33 +119,33 @@ public:
 
   void onRosOutCallback(const rosgraph_msgs::Log& msg)
   {
-      this->onRosOutMsg(msg);
+    this->onRosOutMsg(msg);
   }
 
   void onSmaccTransitionCallback(const smacc_msgs::SmaccTransitionLogEntry& msg)
   {
-      this->onSmaccTransition(msg);
+    this->onSmaccTransition(msg);
   }
 };
 
 //---------------------------------------------------------------------------------------------------
 class TimeoutFailureTestPolicy : public TestPolicy
 {
-  ros::Duration timeout_;
-  ros::Time startTime_;
+  ros::WallDuration timeout_;
+  ros::WallTime startTime_;
 
   virtual void init(XmlRpc::XmlRpcValue& initXmlRpcValue) override
   {
     ROS_INFO("[TimeoutFailureTestPolicy] initializating");
-    startTime_ = ros::Time::now();
-    timeout_ = ros::Duration((double)initXmlRpcValue["duration"]);
+    startTime_ = ros::WallTime::now();
+    timeout_ = ros::WallDuration((double)initXmlRpcValue["duration"]);
 
     ROS_INFO_STREAM("[TimeoutFailureTestPolicy] duration: " << timeout_);
   }
 
   virtual void update()
   {
-    auto now = ros::Time::now();
+    auto now = ros::WallTime::now();
 
     auto ellapsed = now - startTime_;
 
@@ -163,39 +164,46 @@ class ReachedStateSuccessTestPolicy : public TestPolicy
     ROS_INFO("[ReachedStateSuccessTestPolicy] initializating");
     targetStateName_ = ((std::string)initXmlRpcValue["state_name"]);
     ROS_INFO_STREAM("[ReachedStateSuccessTestPolicy] success state: " << targetStateName_);
-    this->owner_->onSmaccTransition.connect(
-    [=](auto& msg)
-    {
-        ROS_INFO_STREAM("[ReachedStateSuccessTestPolicy] received state: " << msg.transition.destiny_state_name);
+    this->owner_->onSmaccTransition.connect([=](auto& msg) {
+      ROS_INFO_STREAM("[ReachedStateSuccessTestPolicy] received state: " << msg.transition.destiny_state_name);
 
-        if(msg.transition.destiny_state_name == targetStateName_)
-        {
-            this->owner_->sucess(std::string("success destiny state ") + targetStateName_);
-        }
+      if (msg.transition.destiny_state_name == targetStateName_)
+      {
+        this->owner_->sucess(std::string("success destiny state ") + targetStateName_);
+      }
     });
   }
 
   virtual void update()
   {
-  
   }
 };
 //---------------------------------------------------------------------------------------------------
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "smacc_runtime_test_node");
+// Bring in gtest
+#include <gtest/gtest.h>
 
+// Declare a test
+TEST(TestSuite, testCase1)
+{
   SmaccTestRuntimeNode testNode;
   testNode.init();
 
   ros::Rate r(20);
 
-  while (ros::ok())
+  while (ros::ok() && !testNode.cleanSuccessExit_)
   {
     testNode.update();
     ros::spinOnce();
     r.sleep();
   }
   // ros::Subscriber sub = nh.subscribe();
+}
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "smacc_runtime_test_node");
+  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
 }
